e07526813d4e231fbdd4b855c42392fa1c3642b7
moonch <cogus2010@naver.com>
2024-03-29 12:34:57 +0900

    Clock 시간 출력

---
+++ my-app/src/Hello.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Hello extends ReadableByteStreamController.Component{
+    render(){
+        return <div>Hello {this.props.toWhat}</div>
+    }
+}
+
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(<Hello toWhat="World"/>)
+export default Hello;
---
+++ my-app/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Clock from './ch04/Clock';
+import './index.css';
+import App from './App';
+import Book from './ch03/Book';
+import Library from './ch03/Library';
+import reportWebVitals from './reportWebVitals';
+// import Hello from './Hello'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+setInterval(() => {
+  root.render(
+    <React.StrictMode>
+      <Clock/>
+    </React.StrictMode>
+  );
+}, 1000);
+
+// root.render(
+//   <React.StrictMode>
+//     <Library />
+//   </React.StrictMode>
+// );
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
